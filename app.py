2aa5e63925a7764b6a342a2ba60683564938de49
Udoy <udoy154@gmail.com>
2025-06-15 14:42:41 +0600

    First example repository and file Pushing to Github

---
+++ app.py
@@ -0,0 +1,15 @@
+# example of normal division will give actual value.
+a = 100 / 3
+print(f"a {a}")
+
+# example of division will give value without dicimal
+a = 100 // 3
+print(f"a {a}")
+
+# example of modulus
+a = 10 % 3
+print(f"a {a}")
+
+# example of power to the right
+a = 10 ** 3
+print(f"a {a}")
